d2121404a89f140f3de339cc24bc5d57910244e4
kup-music <andrewkupke2711@gmail.com>
2021-10-20 13:57:50 +1030

    Added more links

--- mainScript.js
+++ mainScript.js
@@ -5,3 +5,11 @@ function overlayOn() {
 function overlayOff() {
 	document.getElementById("overlay").style.display = "none";
 }
+
+function socialsOn() {
+	document.getElementById("socialsList").style.display = "block";
+}
+
+function socialsOff() {
+	document.getElementById("socialsList").style.display = "none";
+}
